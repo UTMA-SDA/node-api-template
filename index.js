f8680dbe5924c8e22c83d6d0404387252a46f641
Alex HM <alex13pks@gmail.com>
2023-10-03 17:32:21 -0600

    Added firebase config

--- index.js
+++ index.js
@@ -3,8 +3,31 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
 import './env.js';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
 
 const app = express();
+
+// config firebase
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDhu4mr1JuHMGAf8xPUFo16BJZR7tXm9Oc',
+  authDomain: 'utma-sda.firebaseapp.com',
+  projectId: 'utma-sda',
+  storageBucket: 'utma-sda.appspot.com',
+  messagingSenderId: '590082649662',
+  appId: '1:590082649662:web:23c3bfdd23867045162b6f',
+  measurementId: 'G-VDJR68GM9N',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // config bcrypt
 const saltRounds = 15;
